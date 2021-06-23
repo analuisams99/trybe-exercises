45544d50a4d5e43adc75b21cd7242fc36142097f
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-23 16:05:46 -0300

    pratica parte III

--- exercises/Bloco 4/Práticas/4.1/scripts.js
+++ exercises/Bloco 4/Práticas/4.1/scripts.js
@@ -1,9 +1,31 @@
-const name = "Ana Luisa";
-const birthCity = "Pouso Alegre";
-var birthYear = 1999;
-birthYear = 2030;
-birthCity = "Estiva";
+// Parte II
+// const name = "Ana Luisa";
+// const birthCity = "Pouso Alegre";
+// var birthYear = 1999;
+// birthYear = 2030;
 
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// Parte III exemplo
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId) //Mostra o tipo primitivo do valor da variavel
+// console.log(typeof patientAge) 
+
+//Parte III pratica
+var base = 5;
+var altura = 8;
+var area = base * altura;
+var perimetro = base + base + altura + altura;
+
+console.log(area)
+console.log(perimetro)
+
