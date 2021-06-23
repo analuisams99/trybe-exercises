4f24ddbc965406e7a6df763c577db7fa4e08ca71
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-23 20:54:42 -0300

    exercicio 4 de introduçao a js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio-introducao-JS/exercicio_4.js
@@ -0,0 +1,9 @@
+let positivoOuNegativo = -4;
+
+if (positivoOuNegativo > 0){
+  console.log("positive");
+} else if (positivoOuNegativo < 0){
+  console.log("negative");
+} else {
+  console.log("zero");
+};
