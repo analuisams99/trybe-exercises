57de3fcbae74f97934690a4d34c9c63b946f82d1
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-23 20:47:38 -0300

    Exercicio 3 de introduçao a js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio-introducao-JS/exercicio_3.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 10;
+let c = 2;
+
+if (a > b && a > c) {
+  console.log("'a' é maior que 'b' e 'c'");
+} else if (b > c && b > a) {
+  console.log("'b' é maior que 'c' e 'a'");
+} else {
+  console.log("'c' é maior que 'a' e 'b'");
+}
