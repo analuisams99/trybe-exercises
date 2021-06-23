2b67de4fcbda7f070b4e6f5c713afb4e64911ff3
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-23 15:40:46 -0300

    pratica de exercicios

---
+++ exercises/Bloco 4/Práticas/4.1/scripts.js
@@ -0,0 +1,9 @@
+const name = "Ana Luisa";
+const birthCity = "Pouso Alegre";
+var birthYear = 1999;
+birthYear = 2030;
+birthCity = "Estiva";
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
