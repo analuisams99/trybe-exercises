901cca731fc7f484ca3e7b4d4bc1c080ba533d67
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-27 17:57:03 -0300

    exercicio 4

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/dia_2/exercicio4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaNumeros = 0;
+
+for ( let index = 0; index < numbers.length; index += 1){
+  somaNumeros += numbers[index];
+}
+let mediaAritmetica = somaNumeros / numbers.length;
+
+if (mediaAritmetica > 20){
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
