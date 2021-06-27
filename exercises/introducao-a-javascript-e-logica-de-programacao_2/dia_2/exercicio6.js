ad7379251eda815193441415f4a4ddaa9b8e573b
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-06-27 18:30:52 -0300

    exercicio 6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/dia_2/exercicio6.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroImpar = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+  if (numbers[i] % 2 === 1){
+    numeroImpar += 1;
+  }
+} 
+  if (numeroImpar > 0){
+    console.log("existem: " + numeroImpar + " números ímpares")
+  } else {
+    console.log("nenhum valor ímpar encontrado");
+  }
