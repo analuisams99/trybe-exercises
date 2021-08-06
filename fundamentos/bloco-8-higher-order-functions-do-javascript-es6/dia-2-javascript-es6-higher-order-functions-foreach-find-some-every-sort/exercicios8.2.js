a76b7b99102cc11514d2a7a703715a3a613d9a9b
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-08-06 19:17:42 -0300

    Realiza exercicio 6, se algum livro foi lançado na decada de 80

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios8.2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios8.2.js
@@ -155,11 +155,20 @@ const books = [
 
 // assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
 
-// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
-const expectedResult = false;
+// // 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+// const expectedResult = false;
 
-function everyoneWasBornOnSecXX() {
-  return books.every((pessoa) => pessoa.author.birthYear >= 1901 && pessoa.author.birthYear <= 2000);
+// function everyoneWasBornOnSecXX() {
+//   return books.every((pessoa) => pessoa.author.birthYear >= 1901 && pessoa.author.birthYear <= 2000);
+// }
+
+// assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
+
+//6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((livro) => livro.releaseYear >= 1980 && livro.releaseYear <= 1989);
 }
 
-assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
